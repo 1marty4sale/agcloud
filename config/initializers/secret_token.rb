0551c316beb25a08147a4c5d03a7c8a716c93499
Marty Hitchcock <marty@choiceas.com>
2014-05-21 12:34:41 +1200

    Start of Ag Cloud, added todo scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agcloud::Application.config.secret_token = '25397820a2ad000d73ee15d90863e07848c68a5a9a36c1f0977a4c70329eb4dedc30b48b74817848034cefc87a4b2fa0c914c393468119fe1ec526fb4063c95d'
